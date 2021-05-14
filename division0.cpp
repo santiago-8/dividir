cf8158e0eccaa19158b62c54bfbee25f09854093
santiago-8 <santiagodevoto2003@gmail.com>
2021-05-13 22:50:05 -0300

    ahora ya no sale del bucle cuando el resto es 0, ya que agregué la variable resto

--- division0.cpp
+++ division0.cpp
@@ -4,12 +4,12 @@
 
 using namespace std;
 
-int dividendo=1,divisor=1,resultado,diferencia;
+int dividendo=1,divisor=1,resultado,diferencia,resto;
 
 int main()
 {
 	cout << "** ¡Hola! Este es un programa para dividir números naturales **" << endl << endl;		
-	cout <<  "Ingrese 0 en cualquier momento para salir." << endl ;  /* Menú del programa */
+	cout <<  "Ingrese 0 en cualquier momento para salir." << endl ; 	 /* Menú del programa */
 
 	while ( dividendo > 0 && divisor > 0 )					/* Si el dividendo y el divisor son mayores a 0,ejecuta el bloque de instrucciones */
 	{	
@@ -26,19 +26,20 @@ int main()
 	if (divisor < 1 ) break;						/* Si se ingresa 0 o menor como divisor, corta el bucle y sale del programa */
 	
 	cout << endl << dividendo << " / " << divisor;
-	
-		for ( resultado = 0 ; dividendo >= divisor; resultado++)	/* Realiza la operación de división restando sucesivamente */
+
+	resto = dividendo;
+
+		for ( resultado = 0 ; resto >= divisor; resultado++)	/* Realiza la operación de división restando sucesivamente */
 		{
-		       for ( diferencia = 0 ; (diferencia + divisor) < dividendo ; diferencia ++ )	/* Realiza la operación de resta sumando sucesivamente */
+		       for ( diferencia = 0 ; (diferencia + divisor) < resto ; diferencia ++ )	/* Realiza la operación de resta sumando sucesivamente */
 		       {
 		       }
 
-		       dividendo = diferencia;
+		       resto = diferencia;
 		}
 
 	cout <<  " = " << resultado ;						/* Muestra el resultado y el resto */
-	cout << "    Resto : " << dividendo << endl;
-
+	cout << "    Resto : " << resto << endl;
 	}
 return 0;
 }
